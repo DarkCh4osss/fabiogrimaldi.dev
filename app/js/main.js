f6ab36ba1456f24fabb83c6aefcd21eb4885b73d
DarkCh4osss <riboldazzifedeico@gmail.com>
2022-01-27 20:03:15 +0100

    🌺| Contact Section Finished + Footer added

--- app/js/main.js
+++ app/js/main.js
@@ -2,6 +2,11 @@ let navbar = document.querySelector(".hero__navbar");
 let navlinks = document.querySelectorAll(".nav-link");
 let sticky = navbar.offsetTop;
 
+let copyright = document.querySelector(".copyright");
+let year = new Date().getFullYear();
+
+copyright.innerText = year;
+
 window.addEventListener("scroll", () => {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky");
